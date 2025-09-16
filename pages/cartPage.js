fd181c46987b2ff684599b14740d4e787ad16bfc
Wesley Maciel <weslmaciel@gmail.com>
2025-09-16 09:23:30 -0300

    POM da pagina do Carrinho de compras

--- pages/cartPage.js
+++ pages/cartPage.js
@@ -2,7 +2,6 @@ export class CartPage {
     constructor(page) {
         this.page = page;
         this.cartItems = page.locator('.cart_item');
-        this.cartQty = page.locator('div[data-test="item-quantity"]');
         this.continueShoppingBtn = page.locator('button[data-test="continue-shopping"]');
         this.checkoutBtn = page.locator('button[data-test="checkout"]');
         this.removeButtons = page.locator('button[data-test="remove-sauce-labs-backpack"]');
@@ -14,7 +13,20 @@ export class CartPage {
         await this.page.goto('/cart.html');
     }
 
-    async getListProductsInCart() {
-        return await this.listLabelsProducts.allTextContents();
+    async clickCheckout(){
+        await this.checkoutBtn.click();
+    };
+
+    async clickContinueShopping(){
+        await this.continueShoppingBtn.click();
+    };
+
+    async getCartItemsCount(){
+        return await this.cartItems.count();
+    };
+
+    async removeProductFromCart(){
+        await this.removeButtons.click();
     }
+
 }
